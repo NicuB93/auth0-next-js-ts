0121b7bfe005529abf9027f97b1d581a66266649
Nicu <nicanorbalmusnb@gmail.com>
2021-11-18 23:41:11 +0200

    Auth0 added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <a href="/login" className="login">
+      <a href="./api/auth/" className="login">
         Login
       </a>
     </div>
