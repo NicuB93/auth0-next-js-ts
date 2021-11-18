28c3a3a287968ee79cccd09ca23326c0b3308ca4
Nicu <nicanorbalmusnb@gmail.com>
2021-11-19 01:12:15 +0200

    Auth0 update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </UserProvider>
   );
 }
