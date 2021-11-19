5bf93ad0be35768bf8bcf7f967bb5bfc238efbed
Nicu <nicanorbalmusnb@gmail.com>
2021-11-19 02:02:02 +0200

    Auth0 update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
+import Profile from '../components/Profile';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <a href="./api/auth/login" className="login">
-        Login
-      </a>
-      <a href="./api/auth/logout" className="login">
-        Logout
-      </a>
+      <Profile />
     </div>
   );
 };
